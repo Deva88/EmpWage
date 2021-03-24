1731415f2ac75a437b27a7b123017f44631b1f0a
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-24 21:31:38 +0530

    Added: Add part time emp wage using switch case

--- src/com/empwage/EmpWage.java
+++ src/com/empwage/EmpWage.java
@@ -3,6 +3,7 @@ package com.empwage;
 public class EmpWage {
 
 	public static void main(String[] args) {
+
 		// constants
 		final int IS_PART_TIME = 1;
 		final int IS_FULL_TIME = 2;
@@ -11,17 +12,20 @@ public class EmpWage {
 		int empHrs = 0;
 		int salary = 0;
 
-		// checking how many hrs employee has worked
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (IS_FULL_TIME == empCheck)
+		// computing employee daily working hrs
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case IS_FULL_TIME:
 			empHrs = 8;
-		else if (IS_PART_TIME == empCheck)
+			break;
+		case IS_PART_TIME:
 			empHrs = 4;
-		else
+			break;
+		default:
 			empHrs = 0;
+		}
 		salary = empHrs * EMP_RATE_PER_HR;
 		System.out.println("Salary = " + salary);
-
 	}
 
 }
