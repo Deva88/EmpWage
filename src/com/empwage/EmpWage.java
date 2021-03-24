91b789fbcb37b54f157dd6adc510b5e4f2a2023e
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-24 21:26:38 +0530

    Added: Add Part time emp wage

--- src/com/empwage/EmpWage.java
+++ src/com/empwage/EmpWage.java
@@ -3,22 +3,20 @@ package com.empwage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-
 		// constants
+		final int IS_PART_TIME = 1;
+		final int IS_FULL_TIME = 2;
 		final int EMP_RATE_PER_HR = 20;
-		final int IS_FULL_TIME_HRS = 8;
-
 		// variables
-		int isPresent = 1;
+		int empHrs = 0;
 		int salary = 0;
-		int empHrs;
-
-		// computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
 
 		// checking how many hrs employee has worked
-		if (empCheck == isPresent)
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (IS_FULL_TIME == empCheck)
 			empHrs = 8;
+		else if (IS_PART_TIME == empCheck)
+			empHrs = 4;
 		else
 			empHrs = 0;
 		salary = empHrs * EMP_RATE_PER_HR;
