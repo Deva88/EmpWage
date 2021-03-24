88703f468a43b3e9f9255f967f1c431c6fe336bc
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-24 21:22:06 +0530

    Added: Calculating empWage for a day.

--- src/com/empwage/EmpWage.java
+++ src/com/empwage/EmpWage.java
@@ -3,12 +3,26 @@ package com.empwage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
+
+		// constants
+		final int EMP_RATE_PER_HR = 20;
+		final int IS_FULL_TIME_HRS = 8;
+
+		// variables
+		int isPresent = 1;
+		int salary = 0;
+		int empHrs;
+
+		// computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+
+		// checking how many hrs employee has worked
+		if (empCheck == isPresent)
+			empHrs = 8;
 		else
-			System.out.println("Employee is Abscent");
+			empHrs = 0;
+		salary = empHrs * EMP_RATE_PER_HR;
+		System.out.println("Salary = " + salary);
 
 	}
 
